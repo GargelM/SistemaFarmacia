26e15d461a1018f74d03eb61ae159361d4553dc9
Michel Gargel <gargelmichel@hotmail.com>
2018-05-10 20:22:32 -0300

    Criando arquivo para conexao

---
+++ Farmacia/src/br/com/farmacia/domain/Fornecedores.java
@@ -0,0 +1,18 @@
+package br.com.farmacia.domain;
+
+public class Fornecedores {
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	private Long codigo;
+	private String descricao;
+}
