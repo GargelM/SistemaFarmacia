5fc3ce2e50e114fb010fb3182a316f6c59ef6228
Michel Gargel <gargelmichel@hotmail.com>
2018-05-12 22:35:15 -0300

    metodo excluir com testes

--- Farmacia/src/br/com/farmacia/DAO/FornecedoresDAO.java
+++ Farmacia/src/br/com/farmacia/DAO/FornecedoresDAO.java
@@ -13,7 +13,7 @@ public class FornecedoresDAO {
 		StringBuilder sql = new StringBuilder();// Stringbuid usa ele para n ficar concatenando com + valor e etc. ...
 												// ele faz concatenar valores sem ficar utilizando os parametros
 		sql.append("INSERT INTO fornecedores ");
-		sql.append("(descricao)");
+		sql.append("(descricao) ");
 		sql.append("VALUES (?)");
 
 		Connection conexao = ConexaoFactory.conectar();
@@ -23,19 +23,36 @@ public class FornecedoresDAO {
 		comando.executeUpdate();
 
 	}
+	
+	public void excluir(Fornecedores f) throws SQLException{
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM fornecedores ");
+		sql.append("WHERE codigo = ? ");
+		
+		Connection conexao = ConexaoFactory.conectar();
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());//1 primeiro registro
+		comando.executeUpdate();
+		
+	}
+
 
 	public static void main(String[] args) {
-		Fornecedores f1 = new Fornecedores();
-		f1.setDescricao("DESCRICAO 1");
+		//Fornecedores f1 = new Fornecedores();
+		//f1.setDescricao("DESCRICAO 1");
 		
-		Fornecedores f2 = new Fornecedores();
-		f2.setDescricao("DESCICAO 2");
+		//Fornecedores f2 = new Fornecedores();
+		//f2.setDescricao("DESCICAO 2");
+		
+		Fornecedores f3 = new Fornecedores();
+		f3.setCodigo(2L);
 		
 		FornecedoresDAO fdao = new FornecedoresDAO();
 		try {
-			fdao.salvar(f1);
-			fdao.salvar(f2);
-			System.out.println("Salvo com sucesso");
+			//fdao.salvar(f1);
+			//fdao.salvar(f2);
+			fdao.excluir(f3);
+			System.out.println("deledado com sucesso");
 		} catch (SQLException e) {
 			System.out.println("Erro ao salvar");
 			e.printStackTrace();
