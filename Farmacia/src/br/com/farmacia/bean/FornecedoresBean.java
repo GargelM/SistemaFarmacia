1ae4b05dd43ac968b48bd2273798842faf509b7d
Michel Gargel <gargelmichel@hotmail.com>
2018-05-13 21:22:06 -0300

    Classe java para buscar fornecedores

--- Farmacia/src/br/com/farmacia/bean/FornecedoresBean.java
+++ Farmacia/src/br/com/farmacia/bean/FornecedoresBean.java
@@ -1,10 +1,14 @@
 //fazer comunicaçao parte web com banco de dados. java vx xhtml
 package br.com.farmacia.bean;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.model.ListDataModel;
 
+import br.com.farmacia.DAO.FornecedoresDAO;
 import br.com.farmacia.domain.Fornecedores;
 
 @ManagedBean(name = "MBFornecedores") //uma forma de passar referencia  usara para chamar em vez de chamar fornecedoresbean 
@@ -19,6 +23,19 @@ public class FornecedoresBean {
 
 	public void setItens(ListDataModel<Fornecedores> itens) {
 		this.itens = itens;
+	}
+	
+	public void prepararPesquisa() {
+		FornecedoresDAO fdao = new FornecedoresDAO();
+		
+		try {
+			ArrayList<Fornecedores>lista = fdao.listar();
+			itens = new ListDataModel<Fornecedores>(lista);//converter a variavel em datamodel
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
