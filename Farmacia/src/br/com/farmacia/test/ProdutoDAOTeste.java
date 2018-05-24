c43ffb380bcc4d239238d280caaa87a8bee7875b
Michel Gargel <gargelmichel@hotmail.com>
2018-05-24 19:54:30 -0300

    Testando insercao de produto

--- Farmacia/src/br/com/farmacia/test/ProdutoDAOTeste.java
+++ Farmacia/src/br/com/farmacia/test/ProdutoDAOTeste.java
@@ -1,5 +1,32 @@
 package br.com.farmacia.test;
 
-public class ProdutoDAOTeste {
+import java.sql.SQLException;
 
+
+import org.junit.Test;
+
+
+import br.com.farmacia.DAO.ProdutoDAO;
+import br.com.farmacia.domain.Fornecedores;
+import br.com.farmacia.domain.Produtos;
+
+
+public class ProdutoDAOTeste {
+	@Test
+	public void salvar() throws SQLException{
+
+		Produtos p1 = new Produtos();
+		p1.setDescricao("Dipirona");
+		p1.setPreco(2.92);
+		p1.setQuantidade(12L);
+
+		Fornecedores f = new Fornecedores();
+		f.setCodigo(16);
+		p1.setFornecedores(f);
+
+		ProdutoDAO fdao = new ProdutoDAO();
+
+		fdao.salvar(p1);
+
+	}
 }
